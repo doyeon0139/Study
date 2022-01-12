6e73f282b3ca5f0de0f4edca0d47b0a177270f2f
doyeon0139 <ljhye0139@naver.com>
2022-01-12 20:56:21 +0900

    newly update

---
+++ Python example/test.py
@@ -0,0 +1,16 @@
+# String
+# txt1 = 'A tale that was not right'
+# 'ale' 이 출력..
+# 'A tale' 출력
+# 'not' 출력
+# txt1을 소문자 전환
+# txt1을 대문자 전환
+# 'o'가 몇 개 존재하는가?
+# 'o'가 몇 번 인덱스에 존재하는가?
+# txt1의 길이는 어떻게 되는가?
+# space를 기준으로 나누어서 List에 담으세요..
+# 'tale'을 'story'로 바꾸세요..
+# txt1 안에 'right'가 존재하는지 확인
+
+txt1 = 'A tale that was not right'
+txt1[3:6]
